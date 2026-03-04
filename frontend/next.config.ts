d68ebd36fce15306356c920ff6b45f31e155dfc0
ayoub9360 <ayoub.elguendouz@gmail.com>
2026-03-04 15:17:14 +0100

    build: update dependencies versions

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  turbopack: {},
   webpack(config) {
     config.module.rules.push({
       test: /\.woff2$/,
